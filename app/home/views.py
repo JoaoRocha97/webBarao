c926ffff7bfd23f942642af1e4f1f1cbee8efbee
JoaoRocha97 <joao_rocha97@outlook.com>
2018-06-08 09:26:17 -0300

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -3,19 +3,12 @@ from flask_login import login_required
 
 from . import home
 
-
 @home.route('/')
 def homepage():
-    """
-    Render the homepage template on the / route
-    """
-    return render_template('home/index.html', title="Welcome")
+    return render_template('home/index.html', title="Bem Vindo")
 
 
 @home.route('/dashboard')
 @login_required
 def dashboard():
-    """
-    Render the dashboard template on the /dashboard route
-    """
     return render_template('home/dashboard.html', title="Dashboard")
